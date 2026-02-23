e1a3adb0527a536b3b52b7a541b6829b5ad3416e
KUSHAGRA SHARMA <kushagrasharma4222005@gmail.com>
2026-02-23 15:11:55 +0530

    Add find_s algorithm implementation
    Implement find_s algorithm to determine final hypothesis from CSV data.

--- find_s.py
+++ find_s.py
@@ -1,5 +1,29 @@
-finds-starter-template/
+"""finds-starter-template/
 │
 ├── README.md
 ├── find_s.py
-└── sample_output.txt
+└── sample_output.txt"""
+
+import csv
+
+def find_s_algo(filename):
+    with open(filename, 'r') as file:
+        csvreader = csv.reader(file)
+        
+        hypothesis = None
+        
+        for row in csvreader: 
+            attribute = row[:-1]
+            target = row[-1]
+            
+            if target == 'yes':
+                if hypothesis is None:
+                    hypothesis = attribute.copy()
+                else:
+                    for i in range(len(hypothesis)):
+                        if hypothesis[i] != attribute[i]:
+                            hypothesis[i] = '?'
+        return hypothesis
+    
+final_hypothesis = find_s_algo("enjoysport.csv")
+print("Final Hypothesis:", final_hypothesis)
